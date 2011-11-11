2937229413f54dc3d0ee41e4f98695eea85d1df7
Brian Bartholomew <bri@jealousrepublic.com>
2011-11-10 22:25:49 -0500

    Added Basic User Auth. Removed Default Index.html

--- Gemfile
+++ Gemfile
@@ -20,7 +20,7 @@ end
 gem 'jquery-rails'
 
 # To use ActiveModel has_secure_password
-# gem 'bcrypt-ruby', '~> 3.0.0'
+gem "bcrypt-ruby", :require => "bcrypt"
 
 # Use unicorn as the web server
 # gem 'unicorn'
@@ -30,4 +30,6 @@ gem 'jquery-rails'
 
 # To use debugger
 # gem 'ruby-debug'
+
+gem "rake", "0.8.7"
 
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.authenticate(params[:email], params[:password])
+    if user
+      session[:user_id] = user.id
+      redirect_to root_url, :notice => "Logged In"
+    else
+      flash.now.alert = "Something is wrong"
+      render "new"
+    end
+  end
+  
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, :notice => "Logged out"
+  end
+
+end
