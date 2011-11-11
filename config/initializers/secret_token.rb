6327aa4a1bb030f53b5467f3cb979cd77d038437
Brian Bartholomew <bri@jealousrepublic.com>
2011-11-10 21:27:32 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BrickImg::Application.config.secret_token = '921b101e001951880193a15e3db15944573905f898a1aabcf76b2a2fd2361a9a996cf59e9056764a8f942952f25e93a3d8734e17f10329731e25c7e0d73b50be'
